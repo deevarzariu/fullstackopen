5a80b4f4c790bf2bf244a9241c67b518d6f03bf2
deevarzariu <dell.varzariu@gmail.com>
2024-08-04 19:35:33 +0300

    Change the structure of the mock data

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,38 +4,47 @@ const Header = (props) => {
 
 const Content = (props) => {
   return <>
-    <Part name={props.parts[0]} noExercises={props.noExercises[0]} />
-    <Part name={props.parts[1]} noExercises={props.noExercises[1]} />
-    <Part name={props.parts[2]} noExercises={props.noExercises[2]} />
+    {
+      props.parts.map(({ name, exercises }, index) =>
+        <Part name={name} exercises={exercises} key={index} />)
+    }
   </>
 }
 
 const Part = (props) => {
   return <p>
-    {props.name} {props.noExercises}
+    {props.name} {props.exercises}
   </p>
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+
+  return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const total = exercises1 + exercises2 + exercises3
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} noExercises={[exercises1, exercises2, exercises3]} />
-      <Total total={total} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
