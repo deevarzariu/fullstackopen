9e5382118b01c7b9b3c38477fab9cc14ade77e20
Delia Varzariu <delia.varzariu@globant.com>
2024-08-05 11:47:48 +0300

    Implement adding a person to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,29 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleInputChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons([...persons, { name: newName }])
+    setNewName("")
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(({ name }) => <div key={name}>{name}</div>)}
     </div>
   )
 }
