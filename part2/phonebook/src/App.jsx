978ddf7a0844c3c46bbdcff3310087b28dfe167d
Delia Varzariu <delia.varzariu@globant.com>
2024-08-05 12:09:32 +0300

    Add filtering by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterKeyword, setFilterKeyword] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -13,6 +18,11 @@ const App = () => {
 
   const handlePhoneChange = (e) => {
     setNewPhone(e.target.value)
+  }
+
+  const handleFilter = (e) => {
+    setFilterKeyword(e.target.value);
+
   }
 
   const addPerson = (e) => {
@@ -36,9 +46,19 @@ const App = () => {
     return personsWithSameName.length === 0
   }
 
+  const filteredPersons =
+    filterKeyword ?
+      persons.filter(({ name }) => name.toLocaleLowerCase().includes(filterKeyword.toLocaleLowerCase())) :
+      persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input value={filterKeyword} onChange={handleFilter} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,7 +71,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name, phone }) => <div key={name}>{name} {phone}</div>)}
+      {
+        filteredPersons.map(({ id, name, phone }) => <div key={id}>{name} {phone}</div>)
+      }
     </div>
   )
 }
