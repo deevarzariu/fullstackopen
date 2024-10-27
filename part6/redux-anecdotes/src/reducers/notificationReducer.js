667da448aaa59cd94172550e09064a6a9aa1543d
deevarzariu <dell.varzariu@gmail.com>
2024-10-27 19:50:15 +0200

    Create notification reducer

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "render here notification...";
+
+const notificationSlice = createSlice({
+  name: "render here notification...",
+  initialState,
+  reducers: {},
+});
+
+export default notificationSlice.reducer;
